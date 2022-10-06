1f06e7572f38be86b9d51d49e811e05c35f36180
Al Amin <almin.cont@gmail.com>
2022-10-07 01:58:56 +0600

    package setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MyInterviewPointAPI
+
+
+# LifespringintAPI
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`MONGO_DB_URL`
---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// express app
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// route
+app.get("/", (req, res) => {
+  res.status(200).json({ api: "Life Spring API 🌳" });
+});
+
+// connect to DB
+mongoose
+  .connect(`${process.env.MONGO_DB_URL}`)
+  .then(() => {
+    // listen for request
+    app.listen(4000, () => {
+      console.log("I on listen for port 4000 😎");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
